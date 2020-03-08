be797047287b0a94cdb70bf79f7fa8835585210c
Andrey Fadeev <fadeev.andrey@mail.ru>
2020-03-08 11:40:45 +0300

    connection error captured

--- diploma_work.py
+++ diploma_work.py
@@ -24,39 +24,56 @@ class User():
 
     def get_user_friends(self):
         params = self.params
-        response = requests.get(f'https://api.vk.com/method/friends.get?user_id={self.user_id}', params)
-        self.friends_list = response.json()['response']['items']
+        try:
+            response = requests.get(f'https://api.vk.com/method/friends.get?user_id={self.user_id}', params)
+            self.friends_list = response.json()['response']['items']
+        except requests.exceptions.ConnectionError:
+            print('Connection failed. Try again later.')
+            return False
+        else:
+            return True
+                
     
-
     def get_user_groups(self):
         params = self.params
         print('executing request...')
-        response = requests.get(f'https://api.vk.com/method/groups.get?user_id={self.user_id}&extended=1&fields=members_count', params)
         try:
-            self.groups_info_list = response.json()['response']['items']
-        except KeyError:
-            self.groups_info_list = []
+            response = requests.get(f'https://api.vk.com/method/groups.get?user_id={self.user_id}&extended=1&fields=members_count', params)
+        except requests.exceptions.ConnectionError:
+            return False
+        else:
+            try:
+                self.groups_info_list = response.json()['response']['items']
+            except KeyError:
+                self.groups_info_list = []
         print('...')
-        response = requests.get(f'https://api.vk.com/method/groups.get?user_id={self.user_id}', params)
         try:
-            self.groups_id_set = set(response.json()['response']['items'])
-        except KeyError:
-            self.groups_id_set = {}
+            response = requests.get(f'https://api.vk.com/method/groups.get?user_id={self.user_id}', params)
+        except requests.exceptions.ConnectionError:
+            return False
+        else:
+            try:
+                self.groups_id_set = set(response.json()['response']['items'])
+            except KeyError:
+                self.groups_id_set = {}
+        return True
 
 
 
 
-def check_common_groups(eshmargunov):
-    for friend_id in eshmargunov.friends_list:
+def check_common_groups(user):
+    for friend_id in user.friends_list:
         friend = User(friend_id)
-        friend.get_user_groups()
-        time.sleep(0.34)
-        common_set = eshmargunov.groups_id_set.intersection(friend.groups_id_set)
-        eshmargunov.groups_id_set.difference_update(common_set)
+        if friend.get_user_groups() == True:
+            time.sleep(0.34)
+            common_set = user.groups_id_set.intersection(friend.groups_id_set)
+            user.groups_id_set.difference_update(common_set)
+        else:
+            print('Connection failed.')
 
     final_list = []
-    for group in eshmargunov.groups_info_list:
-        if group.get('id') in eshmargunov.groups_id_set:
+    for group in user.groups_info_list:
+        if group.get('id') in user.groups_id_set:
             final_list.append({'name':group.get('name'), 'gid':group.get('id'), 'members_count':group.get('members_count')})
     
     file_writing(final_list)
@@ -65,13 +82,14 @@ def check_common_groups(eshmargunov):
 def file_writing(final_list):
     with open('groups.json', 'w', encoding='utf-8') as file:
         json.dump(final_list, file, ensure_ascii=False, indent=4)
+    print('File "groups.json" created.')
 
 
 
 
 if __name__ == "__main__":
     eshmargunov = User(171691064)
-    eshmargunov.get_user_friends()
-    eshmargunov.get_user_groups()
-    check_common_groups(eshmargunov)
-    print('Program finished')
+    if eshmargunov.get_user_friends() == True:
+        eshmargunov.get_user_groups()
+        check_common_groups(eshmargunov)
+    print('Program finished.')
